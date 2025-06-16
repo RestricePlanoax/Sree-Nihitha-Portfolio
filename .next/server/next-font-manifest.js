cd0df68ceed53d38c4a072ba7a8a80c48b0b8ab6
Vishnu Vardhan <vishnuvardhan@VishnuVardhans-MacBook-Pro.local>
2025-06-17 03:27:16 +0530

    Main C

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/vishnuvardhan/Desktop/portfolio/sree-nihitha-portfolio/Sree-Nihitha-Portfolio/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
